084b80e515bb8a867d36b2ba105e999dafcb47c4
Jacob Scase <jacobscase@hotmail.com>
2020-11-25 16:37:42 +0000

    Initial Commit

---
+++ Hacker News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Jacob Scase on 25/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Jacob Scase on 25/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
