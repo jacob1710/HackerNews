68898ca4a7035195540ab83f9140e9274d28566a
Jacob Scase <jacobscase@hotmail.com>
2020-12-15 21:25:21 +0000

    add on press functionality

--- Hacker News/ContentView.swift
+++ Hacker News/ContentView.swift
@@ -7,23 +7,35 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
-    
+    @Environment(\.openURL) var openURL
     @ObservedObject var networkManager = NetworkManager()
     
     var body: some View {
         NavigationView{
             List(networkManager.posts){ post in
-                HStack {
-                    Text(String(post.points))
-                    Text(post.title)
-                }
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                            .onTapGesture{
+                                openURL(URL(string: post.url!)!)
+                            }
+                            .onLongPressGesture {
+                                actionSheet(theurl: post.url!)
+                            }                        
+                    }
             }
             .navigationBarTitle("News")
         }
         .onAppear(perform: {
             self.networkManager.fetchData()
         })
+    }
+    func actionSheet(theurl: String) {
+        guard let data = URL(string: theurl) else { return }
+            let av = UIActivityViewController(activityItems: [data], applicationActivities: nil)
+            UIApplication.shared.windows.first?.rootViewController?.present(av, animated: true, completion: nil)
     }
 }
 
