26535f275550bd37e370b59e46dc627d6e172de6
Jacob Scase <jacobscase@hotmail.com>
2021-08-01 21:34:38 +0100

    Add constants file

---
+++ Hacker News/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Hacker News
+//
+//  Created by Jacob Scase on 01/08/2021.
+//
+
+import Foundation
+
+struct Constants {
+    static var frontPageUrl = "http://hn.algolia.com/api/v1/search?tags=front_page"
+    static var postUrl = "https://hn.algolia.com/api/v1/items/"
+}
